e4005a35ef9f99b2108e46ec1694c1130488673c
Oat-dev2005 <surapich56@gmail.com>
2026-03-15 16:29:24 +0700

    edit chat

---
+++ client.py
@@ -0,0 +1,544 @@
+import socket
+import threading
+import json
+import traceback
+import customtkinter as ctk
+from tkinter import colorchooser, messagebox
+import hashlib
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+client = None
+menu = None
+menu_emoji = None
+username = ""
+
+EMOJI_FONT = ("Segoe UI Emoji", 16)
+
+emoji_list = [
+    "😀","😃","😄","😁","😆",
+    "😂","🤣","😊","😍","😎",
+    "🥰","😘","😜","🤔","😭",
+    "👍","👏","🙏","🔥","❤️"
+]
+
+user_colors = {}
+messages = {}
+message_labels = {}
+
+# ---------------------------
+# generate user color
+# ---------------------------
+def get_color(name):
+    h = hashlib.md5(name.encode()).hexdigest()
+    return "#" + h[:6]
+
+
+# ---------------------------
+# LOGIN WINDOW
+# ---------------------------
+login = ctk.CTk()
+login.title("Python Chat Login")
+login.geometry("360x300")
+
+title = ctk.CTkLabel(login,text="Python Chat",font=("Arial",26,"bold"))
+title.pack(pady=25)
+
+username_entry = ctk.CTkEntry(login,placeholder_text="Username",width=220)
+username_entry.pack(pady=8)
+
+ip_entry = ctk.CTkEntry(login,placeholder_text="Server IP",width=220)
+ip_entry.pack(pady=8)
+
+port_entry = ctk.CTkEntry(login,placeholder_text="Port",width=220)
+port_entry.insert(0,"5000")
+port_entry.pack(pady=8)
+
+# ---------------------------
+# connect
+# ---------------------------
+def start_chat():
+
+    global client,username
+
+    username = username_entry.get()
+    #host = ip_entry.get()
+    host = "192.168.0.101"
+    port = port_entry.get()
+
+    if username=="" or host=="" or port=="":
+        messagebox.showwarning("Error","Please fill all fields")
+        return
+
+    try:
+        port=int(port)
+        client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        client.connect((host,port))
+
+        # รอข้อความจาก server
+        line = client.recv(1024).decode().strip()
+
+        if line == "NAME":
+            client.send(username.encode())
+
+            # รอผลตรวจชื่อ
+            response = client.recv(1024).decode().strip()
+
+            if response == "NAME_TAKEN":
+                messagebox.showerror("Error", "Username นี้มีคนใช้แล้ว")
+                client.close()
+                return
+            elif response == "OK":
+                login.destroy()
+                open_chat()
+
+    except Exception as e:
+        print("Connection error:", e)
+        traceback.print_exc()
+        messagebox.showerror("Error","Cannot connect to server")
+        return
+
+# ---------------------------
+# CHAT WINDOW
+# ---------------------------
+def open_chat():
+
+    root = ctk.CTk()
+    root.title(f"Python Chat - {username}")
+    root.geometry("760x560")
+
+    # left panel
+    left = ctk.CTkFrame(root,width=200)
+    left.pack(side="left",fill="y",padx=5,pady=5)
+
+    # right panel
+    right = ctk.CTkFrame(root)
+    right.pack(side="right",fill="both",expand=True,padx=5,pady=5)
+
+    label = ctk.CTkLabel(left,text="ONLINE USERS",font=("Arial",16,"bold"))
+    label.pack(pady=10)
+
+    user_list = ctk.CTkTextbox(left,width=180,height=400,font=("Arial",13))
+    user_list.pack(padx=8,pady=5,fill="y")
+    user_list.configure(state="disabled")
+
+    chat_bg_color = "#2b2b2b"
+
+    # chat box
+    chat_box = ctk.CTkScrollableFrame(right,fg_color=chat_bg_color)
+    chat_box.pack(fill="both",expand=True,padx=10,pady=10)
+
+    typing_label = ctk.CTkLabel(right,text="")
+    typing_label.pack()
+
+    # input area
+    input_frame = ctk.CTkFrame(right)
+    input_frame.pack(fill="x",padx=10,pady=5)
+
+    msg_entry = ctk.CTkEntry(input_frame,placeholder_text="Type a message...",font=("Segoe UI Emoji",14))
+    msg_entry.pack(side="left",fill="x",expand=True,padx=5,pady=10)
+
+    # ---------------------------
+    # functions
+    # ---------------------------
+    def exit_chat():
+        try:
+            client.close()
+        except:
+            pass
+        root.destroy()
+
+
+    def send_msg(event=None):
+
+        msg = msg_entry.get()
+
+        if msg:
+            client.send(msg.encode())
+            msg_entry.delete(0,"end")
+
+
+    def send_typing(event=None):
+
+        try:
+            client.send("__typing__".encode())
+        except:
+            pass
+
+    def add_system(text):
+        sys_frame = ctk.CTkFrame(chat_box, fg_color="transparent")
+        sys_frame.pack(fill="x", pady=5)
+
+        sys_label = ctk.CTkLabel(
+            sys_frame,
+            text=text,
+            text_color="orange",
+            font=("Segoe UI Emoji",12,"bold")
+        )
+
+        sys_label.pack(anchor="center")
+
+    def add_chat(text,uid=None,msg_id=None):
+
+        if ":" in text:
+
+        # แยก timestamp
+            if text.startswith("["):
+                time_part = text.split("]")[0] + "]"
+                rest = text.split("] ",1)[1]
+            else:
+                time_part = ""
+                rest = text
+
+            name,message = rest.split(":",1)
+            name=name.strip()
+            message=message.strip()
+
+            key = uid if uid else name
+
+            if key not in user_colors:
+                user_colors[key] = get_color(key)
+
+            if name == username: # ข้อความของเรา
+                bubble_bg = "#2b6cb0"   # สีของตัวเอง
+                anchor = "e"
+                side_msg = ctk.RIGHT
+            else: # ข้อความคนอื่น
+                bubble_bg = "#40444b"   # สีคนอื่น
+                anchor = "w"
+                side_msg = ctk.LEFT
+
+            msg_frame = ctk.CTkFrame(chat_box, fg_color="transparent")
+            msg_frame.pack(fill="x", padx=5, pady=7.5, anchor=anchor)
+
+            bubble_frame = ctk.CTkFrame(msg_frame, fg_color="transparent")
+            bubble_frame.pack(fill="x", anchor=anchor)
+
+            name_frame = ctk.CTkFrame(msg_frame, fg_color="transparent")
+            name_frame.pack(fill="x", anchor=anchor)
+
+            msg = ctk.CTkLabel(
+                bubble_frame,
+                text=message,
+                fg_color=bubble_bg,
+                font=("Segoe UI Emoji",14),
+                corner_radius=15,
+                padx=14,
+                pady=8,
+                wraplength=350,
+                justify="left"
+            )
+
+            time_label = ctk.CTkLabel(
+                bubble_frame,
+                text=time_part,
+                # fg_color="#2f3136",
+                text_color="#aaaaaa",
+                font=("Segoe UI Emoji",12)
+            )
+
+            name_label = ctk.CTkLabel(
+                name_frame,
+                text=name,
+                text_color=user_colors[key],
+                font=("Segoe UI Emoji",12,"bold")
+            )
+
+            msg.pack(side=side_msg, padx=10,pady=5,anchor="n")
+            time_label.pack(side=side_msg, anchor="n")
+            
+            name_frame.pack(fill="x",padx=10,anchor=anchor)
+            name_label.pack(side=side_msg)
+
+            if name == username:
+                msg.bind("<Button-3>", lambda e, mid=msg_id,: open_message_menu(e, mid))
+
+            if msg_id:
+                messages[msg_id] = msg_frame
+                message_labels[msg_id] = msg
+
+        else:
+            add_system(text)
+
+    def edit_message(mid,new_text):
+        if mid not in message_labels:
+            return
+
+        label = message_labels[mid]
+
+        label.configure(text=new_text + " (แก้ไขแล้ว)")
+    
+    def send_delete(mid):
+
+        client.send(json.dumps({
+            "type":"delete",
+            "id":mid
+        }).encode())
+
+    def delete_message(mid):
+
+        if mid not in messages:
+            return
+
+        frame = messages[mid]
+        frame.destroy()
+
+        messages.pop(mid,None)
+        message_labels.pop(mid,None)
+
+
+    def open_message_menu(event, mid):
+
+        global menu
+
+        if menu and menu.winfo_exists():
+            return
+        
+        text = message_labels[mid].cget("text")
+
+        menu = ctk.CTkToplevel(root)
+        menu.focus()
+        menu.geometry(f"120x90+{event.x_root}+{event.y_root}")
+        menu.overrideredirect(True)
+
+        edit_btn = ctk.CTkButton(menu,text="Edit",command=lambda:(menu.destroy(),open_edit_dialog(mid,text)))
+        edit_btn.pack(fill="x",padx=10,pady=5)
+
+        del_btn = ctk.CTkButton(menu,text="Delete",fg_color="red",command=lambda:(menu.destroy(), send_delete(mid)))
+        del_btn.pack(fill="x",padx=10,pady=5)
+
+        menu.bind("<FocusOut>", lambda e: menu.destroy())
+    
+    def open_edit_dialog(mid, old_text):
+
+        win = ctk.CTkToplevel(root)
+        win.title("Edit Message")
+        win.geometry("300x130")
+
+        entry = ctk.CTkEntry(win,width=240)
+        entry.insert(0,old_text)
+        entry.pack(pady=20,padx=20)
+
+        def save():
+            new_text = entry.get()
+
+            client.send(json.dumps({
+                "type":"edit",
+                "id":mid,
+                "text":new_text
+            }).encode())
+
+            win.destroy()
+
+        save_btn = ctk.CTkButton(win,text="Save",command=save)
+        save_btn.pack(pady=10)
+    
+    def change_chat_color():
+
+        color = colorchooser.askcolor()[1]
+
+        if color:
+            chat_bg_color = color
+            chat_box.configure(fg_color=chat_bg_color)
+
+    def update_users(users):
+
+        user_list.configure(state="normal")
+        user_list.delete("1.0","end")
+
+        for user in users:
+
+            name = user["name"]
+            uid = user["uid"]
+
+            if uid not in user_colors:
+                user_colors[uid] = get_color(uid)
+
+            color = user_colors[uid]
+
+            user_list.tag_config(uid, foreground=color)
+            user_list.insert("end", name + "\n", uid)
+
+        user_list.configure(state="disabled")
+
+
+    def reset_ui():
+
+        chat_box.delete("1.0","end")
+        add_system("[SYSTEM] Server ปิดแล้ว")
+
+        user_list.configure(state="normal")
+        user_list.delete("1.0","end")
+        user_list.configure(state="disabled")
+
+        user_colors.clear()
+
+
+    # ---------------------------
+    # emoji picker
+    # ---------------------------
+    def add_emoji(e):
+        msg_entry.insert("end",e)
+
+
+    def open_emoji():
+
+        global menu_emoji
+
+        if menu_emoji and menu_emoji.winfo_exists():
+            return
+
+        menu_emoji = ctk.CTkToplevel(root)
+        menu_emoji.title("Emoji")
+        menu_emoji.geometry("300x230")
+
+        menu_emoji.grab_set()  # ป้องกัน focus หลุด
+
+        frame = ctk.CTkFrame(menu_emoji)
+        frame.pack(padx=10,pady=10)
+
+        row = 0
+        col = 0
+
+        for e in emoji_list:
+
+            btn = ctk.CTkButton(
+                frame,
+                text=e,
+                width=45,
+                height=45,
+                font=("Segoe UI Emoji",18),
+                command=lambda emoji=e:(add_emoji(emoji), menu_emoji.destroy())
+            )
+
+            btn.grid(row=row,column=col,padx=4,pady=4)
+
+            col += 1
+
+            if col == 5:
+                col = 0
+                row += 1
+
+    # ---------------------------
+    # receive thread
+    # ---------------------------
+    def receive():
+
+        buffer=""
+
+        while True:
+
+            try:
+
+                data = client.recv(1024).decode()
+
+                if not data:
+                    root.after(0,reset_ui)
+                    break
+
+                buffer+=data
+
+                while "\n" in buffer:
+
+                    line,buffer = buffer.split("\n",1)
+
+                    packet=json.loads(line)
+
+                    if packet["type"]=="chat":
+                        root.after(0,add_chat,packet["data"],packet.get("uid"),packet.get("id"))
+
+                    elif packet["type"]=="system":
+                        root.after(0,add_chat,"[SYSTEM] "+packet["data"])
+
+                    elif packet["type"]=="users":
+                        root.after(0,update_users,packet["data"])
+
+                    elif packet["type"]=="history":
+
+                        for m in packet["data"]:
+                            root.after(0,add_chat,m["text"],m.get("uid"),m.get("id"))
+
+                    elif packet["type"]=="typing":
+
+                        typing_label.configure(text=f"{packet['user']} is typing...")
+
+                        root.after(
+                            2000,
+                            lambda: typing_label.configure(text="")
+                        )
+
+                    elif packet["type"]=="edit":
+
+                        root.after(
+                            0,
+                            edit_message,
+                            packet["id"],
+                            packet["text"]
+                        )
+
+                    elif packet["type"]=="delete":
+
+                        root.after(
+                            0,
+                            delete_message,
+                            packet["id"]
+                        )
+
+            except (ConnectionResetError, ConnectionAbortedError):
+                root.after(0, reset_ui)
+                break
+            except Exception as e:
+                print("Receive error:", e)
+                traceback.print_exc()
+                root.after(0, reset_ui)
+                break
+
+    # ---------------------------
+    # buttons
+    # ---------------------------
+    exit_btn = ctk.CTkButton(
+        input_frame,
+        text="Exit",
+        fg_color="red",
+        width=70,
+        command=exit_chat
+    )
+    exit_btn.pack(side="left",padx=5)
+
+    emoji_btn = ctk.CTkButton(
+        input_frame,
+        text="😀",
+        width=45,
+        font=("Segoe UI Emoji",18),
+        command=open_emoji
+    )
+    emoji_btn.pack(side="left",padx=5)
+
+    send_btn = ctk.CTkButton(
+        input_frame,
+        text="Send",
+        width=80,
+        command=send_msg
+    )
+    send_btn.pack(side="right",padx=5)
+
+    msg_entry.bind("<Return>",send_msg)
+    msg_entry.bind("<Key>",send_typing)
+
+    thread = threading.Thread(target=receive,daemon=True)
+    thread.start()
+
+    root.protocol("WM_DELETE_WINDOW",exit_chat)
+    root.mainloop()
+
+
+# ---------------------------
+# connect button
+# ---------------------------
+connect_btn = ctk.CTkButton(login,text="Connect",width=180,command=start_chat)
+
+connect_btn.pack(pady=25)
+
+login.mainloop()
+
---
+++ server.py
@@ -0,0 +1,275 @@
+import socket
+import threading
+import json
+import traceback
+import customtkinter as ctk
+import datetime
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.title("Server")
+app.geometry("300x150")
+
+status_label = ctk.CTkLabel(
+    app,
+    text="สถานะ: ปิดเซิร์ฟเวอร์อยู่",
+    font=("Arial", 18),
+    text_color="red"
+)
+status_label.pack(pady=10)
+
+host = "192.168.0.101"
+port = 5000
+
+clients = {}
+lock = threading.Lock()
+
+history = []
+
+server = None
+server_running = False
+
+user_counter = 1
+message_counter = 1
+
+
+def log(msg):
+    # print(msg)
+    status_label.configure(text=msg)
+
+
+def broadcast(msg):
+    for c in list(clients.keys()):
+        try:
+            c.send((msg + "\n").encode())
+        except Exception as e:
+            print("broadcast Error",e)
+            traceback.print_exc()  
+            pass # ถ้า client หลุด ไม่ให้ server crash
+
+
+def send_user_list():
+    users = []
+    for uid in clients.values():
+        name = uid.split("#")[0]
+        users.append({"name": name, "uid": uid})
+
+    packet = json.dumps({
+        "type": "users",
+        "data": users
+    })
+    broadcast(packet)
+
+
+def handle_client(conn):
+
+    global message_counter
+
+    uid = clients[conn]
+    name = uid.split("#")[0]
+
+    while server_running:
+
+        try:
+
+            msg = conn.recv(1024).decode()
+
+            if not msg:
+                break
+
+            msg = msg.strip()
+
+            # typing indicator
+            if msg == "__typing__":
+                broadcast(json.dumps({
+                    "type": "typing",
+                    "user": name
+                }))
+                continue
+
+            try:
+                packet = json.loads(msg)
+
+                # edit message
+                if packet["type"] == "edit":
+
+                    mid = packet["id"]
+                    new_msg = packet["text"]
+
+                    for m in history:
+                        if m["id"] == mid and m["uid"] == uid:
+                            
+                            time = m["time"]
+                            new_text = f"[{time}] {name}: {new_msg}"
+
+                            m["text"] = new_text
+
+                            broadcast(json.dumps({
+                                "type":"edit",
+                                "id":mid,
+                                "text":new_msg
+                            }))
+
+                    continue
+
+                # delete message
+                if packet["type"] == "delete":
+
+                    mid = packet["id"]
+
+                    for m in history:
+                        if m["id"] == mid and m["uid"] == uid:
+
+                            history.remove(m)
+                            broadcast(json.dumps({
+                                "type":"delete",
+                                "id":mid
+                            }))
+
+                    continue
+            except:
+                pass
+
+            time = datetime.datetime.now().strftime("%H:%M")
+
+            text = f"[{time}] {name}: {msg}"
+
+            message_id = message_counter
+            message_counter += 1
+
+            history.append({
+                "id":message_id,
+                "msg": msg,
+                "uid":uid,
+                "time":time
+            })
+
+            packet = json.dumps({
+                "type":"chat",
+                "data":text,
+                "uid":uid,
+                "id":message_id
+            })
+
+            broadcast(packet)
+
+        except Exception as e:
+            print("handle Error",e)
+            traceback.print_exc()
+            break  # หากเกิดข้อผิดพลาด ให้หยุด loop
+
+    with lock:
+        if conn in clients:
+            del clients[conn]
+
+    broadcast(json.dumps({
+        "type": "system",
+        "data": f"{name} left the chat"
+    }))
+
+    send_user_list()
+    conn.close()
+
+
+def server_loop():
+
+    global server, user_counter
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+
+    log("สถานะ: เซิร์ฟเวอร์กำลังทำงาน")
+
+    while server_running:
+
+        try:
+
+            conn, addr = server.accept()
+
+            conn.send("NAME\n".encode())
+            name = conn.recv(1024).decode().strip()
+
+            with lock:
+                existing_names = [uid.split("#")[0] for uid in clients.values()]
+
+            if name in existing_names:
+                conn.send("NAME_TAKEN\n".encode())
+                conn.close()
+                continue
+            else:
+                conn.send("OK\n".encode())
+
+            uid = f"{name}#{user_counter}"
+            user_counter += 1
+
+            with lock:
+                clients[conn] = uid
+
+            packet = json.dumps({
+                "type": "history",
+                "data": history
+            })
+
+            conn.send((packet + "\n").encode())
+
+            broadcast(json.dumps({
+                "type": "system",
+                "data": f"{name} joined the chat"
+            }))
+
+            send_user_list()
+
+            thread = threading.Thread(
+                target=handle_client,
+                args=(conn,),
+                daemon=True
+            )
+            thread.start()
+
+        except Exception as e:
+            print("Serverr Error",e)
+            traceback.print_exc()
+            break  # หากเกิดข้อผิดพลาด ให้หยุด loop
+
+
+def start_server():
+    global server_running
+
+    if server_running:
+        return
+
+    server_running = True
+    threading.Thread(target=server_loop, daemon=True).start()
+
+    status_label.configure(
+        text="สถานะ: Server กำลังทำงานอยู่",
+        text_color="lightgreen"
+    )
+
+
+def stop_server():
+    global server_running, server, history
+
+    server_running = False
+
+    if server:
+        server.close()
+
+    history.clear()
+
+    status_label.configure(
+        text="สถานะ: ปิดเซิร์ฟเวอร์แล้ว",
+        text_color="red"
+    )
+
+
+start_button = ctk.CTkButton(app, text="Start Server", command=start_server)
+start_button.pack(pady=10)
+
+stop_button = ctk.CTkButton(app, text="Stop Server", fg_color="red", command=stop_server)
+stop_button.pack(pady=10)
+
+app.mainloop()
